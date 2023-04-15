5709ff683aeba4932e837eaff8c2835a7d4272cb
cekoit00 <cekoit00@hs-esslingen.de>
2023-04-15 11:38:55 +0200

    first commit

---
+++ src/main/kotlin/com/hse/cedric/kolarik/distributedsystemsproject/DistributedSystemsProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.hse.cedric.kolarik.distributedsystemsproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DistributedSystemsProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<DistributedSystemsProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/hse/cedric/kolarik/distributedsystemsproject/DistributedSystemsProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hse.cedric.kolarik.distributedsystemsproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DistributedSystemsProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
